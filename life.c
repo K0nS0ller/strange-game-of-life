d3c330a7655bee8f8fbcf0a8f424c3168a92a1dd
k0nnsolerr <203224951+K0nS0ller@users.noreply.github.com>
2026-03-22 12:20:56 +0000

    Increase grid size and optimize randomization logic, another rules

--- life.c
+++ life.c
@@ -4,9 +4,12 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+
 //you can change it, if you need to change field size
-#define ROW 100
-#define COLUMN 100
+#define ROW 200
+#define COLUMN 120
+
+#define FRAMETIME 100000
 
 struct block {
     bool alive;
@@ -15,32 +18,18 @@ struct block {
 
 struct block grid[ROW][COLUMN];
 
-int rand_sub;
-
-int randnum(){
-    srand(time(NULL) + rand_sub);
-    int randNum = rand() % 101;
-    return randNum;
-}
-
-void begin(){
-    for(int r = 1; r != ROW; r++){
-        for(int c = 1; c != COLUMN; c++){
-            grid[r][c].alive_around = 0;
-            grid[r][c].alive = false;
-        }
-    }
-
+void begin() {
+    srand(time(NULL));
     for(int r = 1; r < ROW; r++){
         for(int c = 1; c < COLUMN; c++){
-            if(randnum() % 101 < 1){ //chance of block aliving in beginning
+            if(rand() % 101 < 50) {
                 grid[r][c].alive = true;
+            } else {
+                grid[r][c].alive = false;
             }
-            rand_sub++;
         }
     }
 }
-
 
 
 int life;
@@ -83,44 +72,35 @@ void logic(){
     for(int row = 1; row < ROW; row++){
         for(int column = 1; column < COLUMN; column++){
             grid[row][column].alive_around = 0;
-                int r_around = row - 1;
-                int c_around = column - 1;
-                for(r_around; r_around != row + 1; r_around++){
-                    check(r_around, c_around, row, column);
+            for(int c_around = column - 1; c_around <= column + 1; c_around++){
+                for(int r_around = row - 1; r_around <= row + 1; r_around++){
+                    if(r_around == row && c_around == column) continue;
+                    else check(r_around, c_around, row, column);
                 }
-                for(c_around; c_around != column + 1; c_around++){
-                    check(r_around, c_around, row, column);
-                }
-                for(r_around; r_around != row - 1; r_around--){
-                    check(r_around, c_around, row, column);
-                }
-                for(c_around; c_around != column - 1; c_around--){
-                    check(r_around, c_around, row, column);
-                }
+            }
 
-//RULES!!!
-                if(grid[row][column].alive_around == 2){
-                    grid[row][column].alive = true;
-                } else{
-                    grid[row][column].alive = false;
-                }
 
+            //RULES
+
+            if(grid[row][column].alive_around <= 3){
+                grid[row][column].alive = true;
+            }
+            else grid[row][column].alive = false;
         }
     }
 }
 
-float frametime = 1.0;
 
 int main(){
     begin();
     field_print();
-    sleep(frametime);
+    usleep(FRAMETIME);
     do{
         logic();
         field_print();
-        printf("\nживо: %d\n", life);
-        sleep(frametime);
+        printf("\nalive: %d\n", life);
+        usleep(FRAMETIME);
     }while(life > 0);
-    printf("все клетки мертвы\n");
+    printf("all cells are dead\n");
     return 0;
 }
